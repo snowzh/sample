068c8bbc1f0c4b6156990df3662ce643bdfef1d6
SnowZh <snowzh8201@outlook.com>
2018-05-27 03:09:14 +0000

    looking edit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -29,7 +30,8 @@ class UsersController extends Controller
         ]);
 
         Auth::login($user);
-        session()->flash('success', '㢍，您将在这里开启一段新的旅程~');
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
 }
