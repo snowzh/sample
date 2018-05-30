81e90d399ab9676e2e5c91e4a27e384cdb585521
SnowZh <snowzh8201@outlook.com>
2018-05-30 12:02:09 +0800

    Revert "Add user following"
    This reverts commit 04e5d35714552400e14cc5d95e076962dc88fc9e.

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('help', 'StaticPagesController@help')->name('help');
-Route::get('about', 'StaticPagesController@about')->name('about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
@@ -23,6 +23,3 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
-
-Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
-Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
